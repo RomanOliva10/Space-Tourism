929268f0e6b96528d56af044afd825dc87eade2f
RomanOliva10 <85895114+RomanOliva10@users.noreply.github.com>
2021-12-24 18:05:16 -0300

    [Initial Commit]

---
+++ src/components/home/home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.css";
+export default function Home() {
+    return (
+      <main>
+        <div className="space">
+            <div className="info">
+                <h4>SO, YOU WANT TO TRAVEL TO</h4>
+                <h1>SPACE</h1>
+                <p>
+                    Let’s face it; if you want to go to space, you might as well<br/>genuinely go to 
+                    outer space and not hover kind of on the<br/>edge of it. Well sit back, and relax 
+                    because we’ll give you<br/>a truly out of this world experience!
+                </p>
+            </div>
+            <div className="explore">
+                <h1>Explore</h1>
+            </div>
+        </div>
+      </main>
+    );
+  }
